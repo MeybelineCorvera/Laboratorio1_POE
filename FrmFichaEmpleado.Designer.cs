4147aa0ebeb4d057fe7273c076b58eda82a356de
MeybelineCorvera <ccabigail48@gmail.com>
2026-08-18 20:21:56 -0600

    Controles y validaciones POE

--- FrmFichaEmpleado.Designer.cs
+++ FrmFichaEmpleado.Designer.cs
@@ -28,20 +28,226 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblDUI = new System.Windows.Forms.Label();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.lblGenero = new System.Windows.Forms.Label();
+            this.lblFechaIngreso = new System.Windows.Forms.Label();
+            this.lblCargo = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtDUI = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.rbiGenerof = new System.Windows.Forms.RadioButton();
+            this.rbiGenerom = new System.Windows.Forms.RadioButton();
+            this.dtpFechaIngreso = new System.Windows.Forms.DateTimePicker();
+            this.txtCargo = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(83, 16);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(69, 17);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(158, 13);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(247, 22);
+            this.txtNombre.TabIndex = 1;
+            this.txtNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombre_KeyPress);
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellido.Location = new System.Drawing.Point(82, 49);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(71, 17);
+            this.lblApellido.TabIndex = 2;
+            this.lblApellido.Text = "Apellido:";
+            // 
+            // lblDUI
+            // 
+            this.lblDUI.AutoSize = true;
+            this.lblDUI.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDUI.Location = new System.Drawing.Point(109, 81);
+            this.lblDUI.Name = "lblDUI";
+            this.lblDUI.Size = new System.Drawing.Size(39, 17);
+            this.lblDUI.TabIndex = 3;
+            this.lblDUI.Text = "DUI:";
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEdad.Location = new System.Drawing.Point(99, 127);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(50, 17);
+            this.lblEdad.TabIndex = 4;
+            this.lblEdad.Text = "Edad:";
+            // 
+            // lblGenero
+            // 
+            this.lblGenero.AutoSize = true;
+            this.lblGenero.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGenero.Location = new System.Drawing.Point(87, 163);
+            this.lblGenero.Name = "lblGenero";
+            this.lblGenero.Size = new System.Drawing.Size(67, 17);
+            this.lblGenero.TabIndex = 5;
+            this.lblGenero.Text = "Genero:";
+            // 
+            // lblFechaIngreso
+            // 
+            this.lblFechaIngreso.AutoSize = true;
+            this.lblFechaIngreso.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaIngreso.Location = new System.Drawing.Point(9, 198);
+            this.lblFechaIngreso.Name = "lblFechaIngreso";
+            this.lblFechaIngreso.Size = new System.Drawing.Size(139, 17);
+            this.lblFechaIngreso.TabIndex = 6;
+            this.lblFechaIngreso.Text = "Fecha de Ingreso:";
+            // 
+            // lblCargo
+            // 
+            this.lblCargo.AutoSize = true;
+            this.lblCargo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCargo.Location = new System.Drawing.Point(99, 241);
+            this.lblCargo.Name = "lblCargo";
+            this.lblCargo.Size = new System.Drawing.Size(56, 17);
+            this.lblCargo.TabIndex = 7;
+            this.lblCargo.Text = "Cargo:";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(158, 46);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(247, 22);
+            this.txtApellido.TabIndex = 9;
+            this.txtApellido.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtApellido_KeyPress);
+            // 
+            // txtDUI
+            // 
+            this.txtDUI.Location = new System.Drawing.Point(158, 78);
+            this.txtDUI.Name = "txtDUI";
+            this.txtDUI.Size = new System.Drawing.Size(247, 22);
+            this.txtDUI.TabIndex = 10;
+            this.txtDUI.TextChanged += new System.EventHandler(this.txtDUI_TextChanged);
+            this.txtDUI.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDUI_KeyPress);
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(158, 121);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(247, 22);
+            this.txtEdad.TabIndex = 11;
+            this.txtEdad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtEdad_KeyPress);
+            // 
+            // rbiGenerof
+            // 
+            this.rbiGenerof.AutoSize = true;
+            this.rbiGenerof.Location = new System.Drawing.Point(158, 159);
+            this.rbiGenerof.Name = "rbiGenerof";
+            this.rbiGenerof.Size = new System.Drawing.Size(88, 20);
+            this.rbiGenerof.TabIndex = 12;
+            this.rbiGenerof.TabStop = true;
+            this.rbiGenerof.Text = "Femenino";
+            this.rbiGenerof.UseVisualStyleBackColor = true;
+            // 
+            // rbiGenerom
+            // 
+            this.rbiGenerom.AutoSize = true;
+            this.rbiGenerom.Location = new System.Drawing.Point(282, 159);
+            this.rbiGenerom.Name = "rbiGenerom";
+            this.rbiGenerom.Size = new System.Drawing.Size(89, 20);
+            this.rbiGenerom.TabIndex = 13;
+            this.rbiGenerom.TabStop = true;
+            this.rbiGenerom.Text = "Masculino";
+            this.rbiGenerom.UseVisualStyleBackColor = true;
+            // 
+            // dtpFechaIngreso
+            // 
+            this.dtpFechaIngreso.Location = new System.Drawing.Point(158, 198);
+            this.dtpFechaIngreso.Name = "dtpFechaIngreso";
+            this.dtpFechaIngreso.Size = new System.Drawing.Size(247, 22);
+            this.dtpFechaIngreso.TabIndex = 14;
+            // 
+            // txtCargo
+            // 
+            this.txtCargo.Location = new System.Drawing.Point(158, 238);
+            this.txtCargo.Name = "txtCargo";
+            this.txtCargo.Size = new System.Drawing.Size(247, 22);
+            this.txtCargo.TabIndex = 15;
+            this.txtCargo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCargo_KeyPress);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.Plum;
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnGuardar.Location = new System.Drawing.Point(158, 325);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(108, 42);
+            this.btnGuardar.TabIndex = 16;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
             // FrmFichaEmpleado
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtCargo);
+            this.Controls.Add(this.dtpFechaIngreso);
+            this.Controls.Add(this.rbiGenerom);
+            this.Controls.Add(this.rbiGenerof);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtDUI);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.lblCargo);
+            this.Controls.Add(this.lblFechaIngreso);
+            this.Controls.Add(this.lblGenero);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.lblDUI);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FrmFichaEmpleado";
             this.Text = "Ficha de Empleado";
+            this.Load += new System.EventHandler(this.FrmFichaEmpleado_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblDUI;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.Label lblGenero;
+        private System.Windows.Forms.Label lblFechaIngreso;
+        private System.Windows.Forms.Label lblCargo;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtDUI;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.RadioButton rbiGenerof;
+        private System.Windows.Forms.RadioButton rbiGenerom;
+        private System.Windows.Forms.DateTimePicker dtpFechaIngreso;
+        private System.Windows.Forms.TextBox txtCargo;
+        private System.Windows.Forms.Button btnGuardar;
     }
 }
 
--- FrmFichaEmpleado.cs
+++ FrmFichaEmpleado.cs
@@ -16,5 +16,95 @@ namespace U20261631
         {
             InitializeComponent();
         }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string mensaje = "Nombre: " + txtNombre.Text + "\n" +
+                             "Apellido: " + txtApellido.Text + "\n" +
+                             "Fecha de Ingreso: " + dtpFechaIngreso.Value.ToShortDateString() + "\n" +
+                             "Cargo: " + txtCargo.Text;
+
+            MessageBox.Show(mensaje, "Datos Guardados");
+        }
+
+        private void txtNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true; // Bloquea cualquier otra tecla
+                MessageBox.Show("Solo se permiten letras", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void txtApellido_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true; // Bloquea cualquier otra tecla
+                MessageBox.Show("Solo se permiten letras", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void txtCargo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true; // Bloquea cualquier otra tecla
+                MessageBox.Show("Solo se permiten letras", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void txtEdad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Solo se permiten numeros ", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void txtDUI_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Solo se permiten numeros ", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void txtDUI_TextChanged(object sender, EventArgs e)
+        {
+            // Quitar el guion para trabajar solo con los dígitos
+            string digitos = txtDUI.Text.Replace("-", "");
+
+            // Limitar a máximo 9 dígitos (00000000-0)
+            if (digitos.Length > 9)
+            {
+                digitos = digitos.Substring(0, 9);
+            }
+
+            string textoFormateado = digitos;
+
+            // Insertar el guion automáticamente después del octavo dígito
+            if (digitos.Length > 8)
+            {
+                textoFormateado = digitos.Substring(0, 8) + "-" + digitos.Substring(8);
+            }
+
+            if (txtDUI.Text != textoFormateado)
+            {
+                txtDUI.Text = textoFormateado;
+                txtDUI.SelectionStart = txtDUI.Text.Length; // mantiene el cursor al final
+            }
+        }
+
+        private void FrmFichaEmpleado_Load(object sender, EventArgs e)
+        {
+            txtNombre.Text = "Meybeline";
+            txtApellido.Text = "Corvera";
+            txtDUI.Text = "069016961";
+            txtEdad.Text = "20";
+            txtCargo.Text = "Data Analyst";
+        }
     }
 }
